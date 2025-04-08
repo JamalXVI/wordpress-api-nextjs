a9c5a433c2d4d7e2919dbbbbdef0eeb49c77e688
Henrique Arantes Tiraboschi <henrique_arantes_338@hotmail.com>
2025-04-08 19:28:27 -0300

    feat: improve wordpress plugin handling and setup process
    - Added checks to verify if WordPress is installed before proceeding.
    - Updated entrypoint to install and activate necessary plugins conditionally.
    - Refactored Dockerfile to copy plugin files individually and set correct paths.

--- wordpress-api/docker-entrypoint.sh
+++ wordpress-api/docker-entrypoint.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -e
 
+
 sleep 10
 
-if [ ! -f /var/www/html/wp-config.php ]; then
+echo "[ENTRYPOINT] Verificando se o WordPress está instalado..."
+if wp core is-installed --allow-root --path=/var/www/html; then
+    echo "[ENTRYPOINT] WordPress já está instalado."
+else
+    echo "[ENTRYPOINT] WordPress não instalado. Por favor, instale o WordPress primeiro."
     exit 1
 fi
 
-cd /var/www/html/wp-content/plugins/tarefas-cpt-api
-composer install --no-interaction --prefer-dist
-
+echo "[ENTRYPOINT] Instalando e ativando o plugin JWT Authentication for WP REST API..."
 wp plugin install jwt-authentication-for-wp-rest-api --activate --allow-root --path=/var/www/html
 
-if wp plugin is-installed tarefas-cpt-api --allow-root --path=/var/www/html ; then
+if wp plugin is-installed tarefas-cpt-api --allow-root --path=/var/www/html; then
+    echo "[ENTRYPOINT] Ativando o plugin customizado 'tarefas-cpt-api'..."
     wp plugin activate tarefas-cpt-api --allow-root --path=/var/www/html
+else
+    echo "[ENTRYPOINT] Plugin 'tarefas-cpt-api' não encontrado, pulando ativação."
 fi
 
+echo "[ENTRYPOINT] Setup concluído. Iniciando o Apache..."
 exec apache2-foreground
